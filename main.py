277c776f883dd43ac3164d962d7d9d87b5fbb25e
Marty <martyumlas@gmail.com>
2025-08-04 22:37:02 +0800

    set up .env in python

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+gemini_key = os.getenv("GEMINI_API_KEY")
+
+print(gemini_key)
+
 print("Hi, I am Albert, how can I help you today? ")
 while True:
     user_input = input("You: ")
     if user_input == 'exit':
         break
     print(f"Cool, thanks for sharing that {user_input}")
+
+
+#AIzaSyDnqhO6KzQ66lt1DDsGiV4ephyXvfI-cfU
