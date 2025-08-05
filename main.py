603cf58a4ac3371fcccbdc9b22e1cdfc5641d951
Marty <martyumlas@gmail.com>
2025-08-05 21:37:48 +0800

    using langchain to get ai replies

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from dotenv import load_dotenv
 import os
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
 gemini_key = os.getenv("GEMINI_API_KEY")
 
-print(gemini_key)
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=gemini_key,
+    temperature=0.5
+)
 
-print("Hi, I am Albert, how can I help you today? ")
-while True:
-    user_input = input("You: ")
-    if user_input == 'exit':
-        break
-    print(f"Cool, thanks for sharing that {user_input}")
+response = llm.invoke([{'role': 'user', 'content': 'Hi there, how are your?'}])
+print(response.content)
+
+# print("Hi, I am Albert, how can I help you today? ")
+# while True:
+#     user_input = input("You: ")
+#     if user_input == 'exit':
+#         break
+#     print(f"Cool, thanks for sharing that {user_input}")
 
 
-#AIzaSyDnqhO6KzQ66lt1DDsGiV4ephyXvfI-cfU
